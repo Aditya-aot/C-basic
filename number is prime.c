f68c0f5a3f816c4399d7b898681839c77ce3179d
Aditya <67204555+Aditya-aot@users.noreply.github.com>
2024-08-05 02:29:05 +0530

    Add files via upload

---
+++ number is prime.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int is_prime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (is_prime(num)) {
+        printf("%d is prime\n", num);
+    } else {
+        printf("%d is not prime\n", num);
+    }
+    return 0;
+}
