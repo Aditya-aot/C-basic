d2cefc7edd1df06e720d3b76909feb3c70f5a6bf
Aditya <67204555+Aditya-aot@users.noreply.github.com>
2024-08-05 02:30:44 +0530

    Add files via upload

---
+++ average of an array of numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+    float average;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+    average = (float)sum / n;
+    printf("Average = %.2f\n", average);
+    return 0;
+}
